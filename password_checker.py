cbb91792fe8eb833ae1af0954cc9d68b890ccad4
GuruKool <pythongurukool@gmail.com>
2026-08-25 21:38:26 +0530

    Created a password checked module

---
+++ password_checker.py
@@ -0,0 +1,21 @@
+
+def check_password_strength(password):
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    for ch in password:
+        if ch.isupper():
+            has_upper = True
+        elif ch.islower():
+            has_lower = True
+        elif ch.isdigit():
+            has_digit = True
+    if len(password) >= 8 and has_upper and has_lower and has_digit:
+        return "Strong"
+    return "Weak"
+
+
+pwd = input("Enter your password")
+result = check_password_strength(pwd)
+
+print("Strength of Password is :: ", result)
