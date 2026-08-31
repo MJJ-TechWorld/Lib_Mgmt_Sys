ecafb8ef2c09e364cb5cc4cb253b292b2973ebed
GuruKool <pythongurukool@gmail.com>
2026-09-01 00:05:37 +0530

    Implemented Add Book Functionality

--- Build_day/Lib_Mgmt_system/main.py
+++ Build_day/Lib_Mgmt_system/main.py
@@ -16,13 +16,56 @@ def menu():
 6. Exit''')
     
 
-# Initialize colorama (autoreset=True reverts to default color after every print)
+def read_book():
+    with open("book_data.txt", "r") as f:
+        book_data = f.readlines()
+    return book_data
+
+def add_book():
+    """This is a add book Function . It takes Book Id, Book Name, Author,Quantity as input """
+    book_data = read_book()
+    print(Fore.GREEN + "--- ADD BOOK ---" )
+    while True:
+        book_id = input("Enter Book Id  :").strip()
+        if book_id.isdigit():
+            book_id = int(book_id)
+            break
+        else:
+            print(Fore.RED + "Invalid Book Id \n It should be a number only")
+
+    book_name = input("Enter the Book name  :")
+    book_author = input("Enter author Name Please : ")
+
+    
+    error_message = Fore.RED + "Invalid quantity entered \n Please Enter a valid Value"
+    while True:
+        book_quantity = input("Enter Quantity :").strip()
+        if book_quantity.isdigit() :
+            if int(book_quantity) > 0:
+                book_quantity = int(book_quantity)
+                break
+            else:
+                print(error_message)    
+        else:
+            print(error_message)
+    print(Fore.GREEN + f'{"Book added successfully.":^100}')
+
+    with  open("book_data.txt", "a") as f:
+        f.write(f"{book_id}, {book_name}, {book_author}, {book_quantity} \n")
+    
+
+
+
+
+    
+
+
 
 while True:
-    menu()
+    menu()        
     choice = input("Enter your choice between 1 to 6 :: ")
     if choice == "1":
-        print("Add Book")
+        add_book()
     elif choice == "2":
         print("View Book")
     elif choice == "3":
